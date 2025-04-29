aeabc0989c7745ee271617ce344a8d0707b6a890
Giulia Franco <giulia@wabrimac.localdomain>
2025-04-29 15:36:09 +0200

    new folder

---
+++ Pipeline_jenkins/README.md
@@ -0,0 +1,4 @@
+# Creazione Dockerfile app Flask
+
+## Descrizione progetto
+Questo progetto contiene un'applicazione Flask scritta in Python, containerizzata tramite Docker, che espone una semplice pagina web con la stringa: "Hello Word"
---
+++ Pipeline_jenkins/app.py
@@ -0,0 +1,16 @@
+# Importa il modulo flask
+from flask import Flask
+
+# Creazione istanza app falsk
+app = Flask(__name__)
+
+# Definizione rotta
+# La funzione hello() viene eseguita quando un utente visita la home page
+@app.route('/')
+def hello():
+    return "Hello World!!" 
+
+# Avviare il server solo se si sta eseguendo questo file 
+# Server accessibile da qualsiasi indirizzo IP sulla porta 8000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
