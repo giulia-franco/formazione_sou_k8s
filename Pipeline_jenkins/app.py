9012f6827e5c581e0fd952fabf15a47b9d8ccbaf
Giulia Franco <giulia@wabrimac.localdomain>
2025-05-08 14:31:03 +0200

    branch

--- Pipeline_jenkins/app.py
+++ Pipeline_jenkins/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # La funzione hello() viene eseguita quando un utente visita la home page
 @app.route('/')
 def hello():
-    return "Hello World from main!!" 
+    return "Hello World from develop!!" 
 
 # Avviare il server solo se si sta eseguendo questo file 
 # Server accessibile da qualsiasi indirizzo IP sulla porta 8000
